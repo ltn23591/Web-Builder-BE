543049c0e16dcda5d8ec6dfbadb84ca2d61ab526
ltn23591 <ltn23591@gmail.com>
2026-05-23 13:51:32 +0700

    config: add auth message

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,8 +1,16 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '@prisma/client';
+import { createAuthMiddleware, APIError } from 'better-auth/api';
 
 const prisma = new PrismaClient();
+
+interface AuthResponse {
+  body?: {
+    code?: string;
+    message?: string;
+  };
+}
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,5 +18,50 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  trustedOrigins: ['http://localhost:3000'],
+  advanced: {
+    disableOriginCheck: process.env.NODE_ENV !== 'production',
+  },
+  hooks: {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    after: createAuthMiddleware(async (ctx) => {
+      const response = ctx.context.returned as AuthResponse | undefined;
+
+      if (!response || !response.body) return;
+
+      // Xử lý lỗi Đăng nhập
+      if (
+        ctx.path.startsWith('/sign-in') &&
+        response.body.code === 'INVALID_EMAIL_OR_PASSWORD'
+      ) {
+        throw new APIError('UNAUTHORIZED', {
+          code: response.body.code,
+          message: 'Tên đăng nhập hoặc mật khẩu không chính xác',
+        });
+      }
+
+      // Xử lý lỗi Đăng ký
+      if (
+        ctx.path.startsWith('/sign-up') &&
+        response.body.code === 'USER_ALREADY_EXISTS'
+      ) {
+        throw new APIError('BAD_REQUEST', {
+          code: response.body.code,
+          message: 'Email này đã được sử dụng!',
+        });
+      }
+
+      // Có thể thêm các mã lỗi khác ở đây
+      if (
+        ctx.path.startsWith('/sign-up') &&
+        response.body.code === 'INVALID_EMAIL'
+      ) {
+        throw new APIError('BAD_REQUEST', {
+          code: response.body.code,
+          message: 'Định dạng email không hợp lệ!',
+        });
+      }
+    }),
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,4 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
